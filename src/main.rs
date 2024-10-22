aaf1e1370a04f033ed1dbea3b2b4a0906e162fe1
Ryan Hasmat <ryan.hasmat@gmail.com>
2024-10-22 14:00:12 -0400

    read other field info

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::mem::MaybeUninit;
 pub struct _SyscallMetaEntry<'a> {
     _type: &'a str,
     name: &'a str,
-    offset: u32,
-    size: u32,
-    signed: u32
+    offset: &'a str,
+    size: &'a str,
+    signed: &'a str,
 }
 
 #[derive(Default, Debug)]
@@ -41,58 +41,101 @@ fn main() {
         let lines = fs::read(format_path);
         let lines = match lines {
             Ok(v) => v,
-            Err(e) => vec![0u8]
+            Err(e) => vec![0u8],
         };
         let lines = std::str::from_utf8(&lines);
         let lines = match lines {
             Ok(v) => v,
-            Err(e) => "error"
+            Err(e) => "error",
         };
         for line in lines.lines() {
             let line = line.trim();
             if line.starts_with("field") {
-                println!("line: {}", line);
                 if (meta.common.len() < 5) {
                     let tk: Vec<String> = line.split(";").map(|c| c.to_string()).collect();
-                    if (tk.len() < 4) { continue; }
-                    let mut meta_ent =  _SyscallMetaEntry {
-                        ..Default::default()
-                    };
+                    if (tk.len() < 4) {
+                        continue;
+                    }
+                    let mut type_buff = [0u8; 128];
+                    let mut name_buff = [0u8; 128];
+                    let mut offset_buff = [0u8; 128];
+                    let mut size_buff = [0u8; 128];
+                    let mut signed_buff = [0u8; 128];
                     for item in tk {
+                        let item = item.trim();
                         if item.starts_with("field") {
-                            let li =  item.len();
-                            let mut sp = 0;
-                            let mut type_buff = [0u8; 128];
-                            let mut name_buff = [0u8; 128];
+                            let li = item.len();
                             for i in 0..li {
                                 if item.chars().nth(li - 1 - i).unwrap() == ' ' {
-                                    println!("item: {}", item);
-                                    println!("space at: {} (li - 1 - i)", (li - 1 - i));
-                                    for j in  (li - 1 - i)..li {
-                                        name_buff[j.wrapping_sub(li - 1 - i)] = item.chars().nth(j).unwrap() as u8;
+                                    for j in (li - i)..li {
+                                        name_buff[j.wrapping_sub(li - i)] =
+                                            item.chars().nth(j).unwrap() as u8;
                                     }
-                                    for j in  0..(li - 1 - i)  {
+                                    for j in 0..(li - 1 - i) {
                                         type_buff[j] = item.chars().nth(j).unwrap() as u8;
+                                    }
+
+                                    break;
+                                }
+                            }
+                        }
+
+                        if item.starts_with("offset") {
+                            let li = item.len();
+                            for i in 0..li {
+                                if item.chars().nth(i).unwrap() == ':' {
+                                    for j in i + 1..li {
+                                        offset_buff[j] = item.chars().nth(j).unwrap() as u8;
                                     }
                                     break;
                                 }
                             }
-                            println!("item starts with field: name_buff[j] = {}, type_buff[j] = {}", std::str::from_utf8(&name_buff).unwrap(), std::str::from_utf8( &type_buff ).unwrap());
-                        }
-
-                        if item.starts_with("offset") {
-                            println!("item starts with offset");
                         }
 
                         if item.starts_with("size") {
-                            println!("item starts with size");
+                            let li = item.len();
+                            for i in 0..li {
+                                if item.chars().nth(i).unwrap() == ':' {
+                                    for j in i + 1..li {
+                                        size_buff[j] = item.chars().nth(j).unwrap() as u8;
+                                    }
+                                    break;
+                                }
+                            }
                         }
 
                         if item.starts_with("signed") {
-                            println!("item starts with signed");
+                            let li = item.len();
+                            for i in 0..li {
+                                if item.chars().nth(i).unwrap() == ':' {
+                                    for j in i + 1..li {
+                                        signed_buff[j] = item.chars().nth(j).unwrap() as u8;
+                                    }
+                                    break;
+                                }
+                            }
                         }
                     }
-                    // meta.common.push(meta_ent);
+                    let mut meta_ent = _SyscallMetaEntry {
+                        name: std::str::from_utf8(&name_buff)
+                            .unwrap()
+                            .trim_end_matches(|c| c == '\0'),
+                        _type: std::str::from_utf8(&type_buff)
+                            .unwrap()
+                            .trim_end_matches(|c| c == '\0'),
+                        offset: std::str::from_utf8(&offset_buff)
+                            .unwrap()
+                            .trim_end_matches(|c| c == '\0')
+                            .trim_start_matches(|c| c == '\0'),
+                        size: std::str::from_utf8(&size_buff)
+                            .unwrap()
+                            .trim_end_matches(|c| c == '\0')
+                            .trim_start_matches(|c| c == '\0'),
+                        signed: std::str::from_utf8(&signed_buff)
+                            .unwrap()
+                            .trim_end_matches(|c| c == '\0')
+                            .trim_start_matches(|c| c == '\0'),
+                    };
                     println!("meta_ent: {:?}", meta_ent);
                 }
             }
