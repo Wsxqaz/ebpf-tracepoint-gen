42cb724f44739f60a21bf5eaef883e46d0713981
Ryan Hasmat <ryan.hasmat@gmail.com>
2024-10-22 23:00:25 -0400

    fields + event name in _SyscallMeta

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ use std::mem::MaybeUninit;
 
 #[derive(Default, Debug)]
 pub struct _SyscallMetaEntry<'a> {
-    _type: &'a str,
-    name: &'a str,
-    offset: &'a str,
-    size: &'a str,
-    signed: &'a str,
+    _type: Box<&'a str>,
+    name: Box<&'a str>,
+    offset: Box<&'a str>,
+    size: Box<&'a str>,
+    signed: Box<&'a str>,
 }
 
 #[derive(Default, Debug)]
 pub struct _SyscallMeta<'a> {
-    common: Vec<_SyscallMetaEntry<'a>>,
+    file: &'a str,
     fields: Vec<_SyscallMetaEntry<'a>>,
 }
 
@@ -24,6 +24,7 @@ pub struct SyscallMeta<'a> {
 }
 
 static SYSCALL_EVENTS_DIR: &str = "/sys/kernel/tracing/events/syscalls";
+
 
 fn main() {
     let _syscall_meta = read_syscall_meta("read");
@@ -37,8 +38,7 @@ fn main() {
         let format_path = format!("{}/format", _path.display());
         println!("format file path: {format_path}");
 
-        let mut meta: _SyscallMeta = unsafe { MaybeUninit::zeroed().assume_init() };
-        let lines = fs::read(format_path);
+        let lines = fs::read(format_path.clone());
         let lines = match lines {
             Ok(v) => v,
             Err(e) => vec![0u8],
@@ -48,99 +48,134 @@ fn main() {
             Ok(v) => v,
             Err(e) => "error",
         };
+
+        let mut fields: Vec<_SyscallMetaEntry> = vec![];
+
         for line in lines.lines() {
             let line = line.trim();
             if line.starts_with("field") {
-                if (meta.common.len() < 5) {
-                    let tk: Vec<String> = line.split(";").map(|c| c.to_string()).collect();
-                    if (tk.len() < 4) {
-                        continue;
-                    }
-                    let mut type_buff = [0u8; 128];
-                    let mut name_buff = [0u8; 128];
-                    let mut offset_buff = [0u8; 128];
-                    let mut size_buff = [0u8; 128];
-                    let mut signed_buff = [0u8; 128];
-                    for item in tk {
-                        let item = item.trim();
-                        if item.starts_with("field") {
-                            let li = item.len();
-                            for i in 0..li {
-                                if item.chars().nth(li - 1 - i).unwrap() == ' ' {
-                                    for j in (li - i)..li {
-                                        name_buff[j.wrapping_sub(li - i)] =
-                                            item.chars().nth(j).unwrap() as u8;
-                                    }
-                                    for j in 0..(li - 1 - i) {
-                                        type_buff[j] = item.chars().nth(j).unwrap() as u8;
-                                    }
-
-                                    break;
-                                }
-                            }
-                        }
-
-                        if item.starts_with("offset") {
-                            let li = item.len();
-                            for i in 0..li {
-                                if item.chars().nth(i).unwrap() == ':' {
-                                    for j in i + 1..li {
-                                        offset_buff[j] = item.chars().nth(j).unwrap() as u8;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-
-                        if item.starts_with("size") {
-                            let li = item.len();
-                            for i in 0..li {
-                                if item.chars().nth(i).unwrap() == ':' {
-                                    for j in i + 1..li {
-                                        size_buff[j] = item.chars().nth(j).unwrap() as u8;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-
-                        if item.starts_with("signed") {
-                            let li = item.len();
-                            for i in 0..li {
-                                if item.chars().nth(i).unwrap() == ':' {
-                                    for j in i + 1..li {
-                                        signed_buff[j] = item.chars().nth(j).unwrap() as u8;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    let mut meta_ent = _SyscallMetaEntry {
-                        name: std::str::from_utf8(&name_buff)
-                            .unwrap()
-                            .trim_end_matches(|c| c == '\0'),
-                        _type: std::str::from_utf8(&type_buff)
-                            .unwrap()
-                            .trim_end_matches(|c| c == '\0'),
-                        offset: std::str::from_utf8(&offset_buff)
-                            .unwrap()
-                            .trim_end_matches(|c| c == '\0')
-                            .trim_start_matches(|c| c == '\0'),
-                        size: std::str::from_utf8(&size_buff)
-                            .unwrap()
-                            .trim_end_matches(|c| c == '\0')
-                            .trim_start_matches(|c| c == '\0'),
-                        signed: std::str::from_utf8(&signed_buff)
-                            .unwrap()
-                            .trim_end_matches(|c| c == '\0')
-                            .trim_start_matches(|c| c == '\0'),
-                    };
-                    println!("meta_ent: {:?}", meta_ent);
-                }
+                let ent = gen_field(line);
+                fields.push(ent);
             }
         }
+
+        let syscall_meta: _SyscallMeta = _SyscallMeta {
+            file: &format_path,
+            fields: fields
+        };
+
+        println!("syscall_meta: {:?}", syscall_meta);
     }
+}
+
+fn gen_field<'a>(line: &str) -> _SyscallMetaEntry {
+                let tk: Vec<String> = line.split(";").map(|c| c.to_string()).collect();
+                if (tk.len() < 4) {
+                    return _SyscallMetaEntry { .. Default::default() };
+                }
+                let mut type_buff = Box::new([0u8; 128]);
+                let mut name_buff = Box::new([0u8; 128]);
+                let mut offset_buff = Box::new([0u8; 128]);
+                let mut size_buff = Box::new([0u8; 128]);
+                let mut signed_buff = Box::new([0u8; 128]);
+                for item in tk {
+                    let item = item.trim();
+                    if item.starts_with("field") {
+                        let li = item.len();
+                        for i in 0..li {
+                            if item.chars().nth(li - 1 - i).unwrap() == ' ' {
+                                for j in (li - i)..li {
+                                    name_buff[j.wrapping_sub(li - i)] =
+                                        item.chars().nth(j).unwrap() as u8;
+                                }
+                                for j in 0..(li - 1 - i) {
+                                    type_buff[j] = item.chars().nth(j).unwrap() as u8;
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+
+                    if item.starts_with("offset") {
+                        let li = item.len();
+                        for i in 0..li {
+                            if item.chars().nth(i).unwrap() == ':' {
+                                for j in i + 1..li {
+                                    offset_buff[j] = item.chars().nth(j).unwrap() as u8;
+                                }
+                                break;
+                            }
+                        }
+                    }
+
+                    if item.starts_with("size") {
+                        let li = item.len();
+                        for i in 0..li {
+                            if item.chars().nth(i).unwrap() == ':' {
+                                for j in i + 1..li {
+                                    size_buff[j] = item.chars().nth(j).unwrap() as u8;
+                                }
+                                break;
+                            }
+                        }
+                    }
+
+                    if item.starts_with("signed") {
+                        let li = item.len();
+                        for i in 0..li {
+                            if item.chars().nth(i).unwrap() == ':' {
+                                for j in i + 1..li {
+                                    signed_buff[j] = item.chars().nth(j).unwrap() as u8;
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+                let leaked_name_buff = Box::leak(name_buff);
+                let mut name = Box::new(
+                    std::str::from_utf8(leaked_name_buff)
+                        .unwrap()
+                        .trim_end_matches(|c| c == '\0')
+                );
+                let leaked_type_buff = Box::leak(type_buff);
+                let mut _type = Box::new(
+                    std::str::from_utf8(leaked_type_buff)
+                        .unwrap()
+                        .trim_end_matches(|c| c == '\0')
+                );
+                let leaked_offset_buff = Box::leak(offset_buff);
+                let mut offset = Box::new(
+                    std::str::from_utf8(leaked_offset_buff)
+                        .unwrap()
+                        .trim_end_matches(|c| c == '\0')
+                        .trim_start_matches(|c| c == '\0')
+                );
+                let leaked_size_buff = Box::leak(size_buff);
+                let mut size = Box::new(
+                    std::str::from_utf8(leaked_size_buff)
+                        .unwrap()
+                        .trim_end_matches(|c| c == '\0')
+                        .trim_start_matches(|c| c == '\0')
+                );
+                let leaked_signed_buff = Box::leak(signed_buff);
+                let mut signed = Box::new(
+                    std::str::from_utf8(leaked_signed_buff)
+                        .unwrap()
+                        .trim_end_matches(|c| c == '\0')
+                        .trim_start_matches(|c| c == '\0')
+                );
+
+
+                let mut meta_ent = _SyscallMetaEntry {
+                    name: name,
+                    _type: _type,
+                    offset: offset,
+                    size: size,
+                    signed: signed,
+                };
+                meta_ent
 }
 
 fn read_syscall_meta(syscall: &str) -> SyscallMeta {
